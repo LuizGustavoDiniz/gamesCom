5174949fb10bf461b8ed3a9ca1159babc66f066e
luizgustavodiniz <gustavo.diniz.gon@gmail.com>
2022-08-22 15:59:07 -0300

    implementação do axios, e requisição dos games

--- controllers/AllGamesController.js
+++ controllers/AllGamesController.js
@@ -1,5 +1,22 @@
+const api = require('../services/api')
+
 module.exports = class AllGamesController{
     static async allGames(req, res){
-        res.render('games/home')
+
+        try {
+            
+            const {data} = await api.get('games',{
+                params:{
+                    category: "shooter"
+                }
+            })
+
+            res.render('games/home',{
+                games: data
+            })
+
+        } catch (error) {
+            res.redirect('/')
+        }
     }
 }
